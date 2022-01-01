582fb2138ba8ad95c7f1a3ba6b6feabd96c33079
Cienna97 <cmortenson97@gmail.com>
2022-01-01 15:43:01 -0800

    adding router get requests

--- db/file.js
+++ db/file.js
@@ -1,1 +1,27 @@
 import util from 'util';
+const fs = require('fs');
+
+ids. https://www.npmjs.com/package/uniqid
+const unidid = require('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = new Store();
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,17 @@
+// require express
 const path = require('path');
 const router = require('express').Router();
 
+
+
+// get notes from notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join)
-})
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// route to fetch from index.html file
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
