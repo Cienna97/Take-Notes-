bb2786e530013cf1cbb9662571c6325605597aa0
Cienna97 <cmortenson97@gmail.com>
2022-01-01 19:13:48 -0800

    adding logic to api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const store = require('../db/file');
 
-router.get('/notes', (req, res))
+
+//
+router.get('/notes', (req, res) => {
+    file
+    .getNotes()
+    .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
 
 
