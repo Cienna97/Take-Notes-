74d50ae62fe3339d9293a86add65ac7c96ae9e4d
Cienna97 <cmortenson97@gmail.com>
2022-01-01 19:41:17 -0800

    apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,4 +12,14 @@ router.get('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
+router.post('/notes', (req, res) => {
+    file
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+    });
 
+
+});
+
+module.export = router;
