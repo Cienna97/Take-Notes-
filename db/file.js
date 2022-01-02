0bfbd4ecb076e697fc599a9cf06c30a39123f61c
Cienna97 <cmortenson97@gmail.com>
2022-01-01 20:10:53 -0800

    writing return statements for database

--- db/file.js
+++ db/file.js
@@ -2,7 +2,30 @@ import util from 'util';
 const fs = require('fs');
 
 
-const  = require('')
+const uuidv1 = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = ulti.promisify(fs.writeFile);
+
+
+class file {
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFileAsync('db/db.json',
+        JSON.stringify(note));
+    }
+
+    getNotes(){
+        return this.read().then((notes) =>{
+            let parsedNotes;
+        })
+    }
+}
+
+
 
 
 
